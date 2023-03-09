88cb74e0c1bb05b6d695368fc1775cb181e5df56
Aleksander Røder <fylling8@gmail.com>
2023-03-09 10:21:54 +0100

    init: spring project with folder structure

---
+++ NoroffAlumni/src/main/java/no/experisAcadmey/trondheim/NoroffAlumni/NoroffAlumniApplication.java
@@ -0,0 +1,13 @@
+package no.experisAcadmey.trondheim.NoroffAlumni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoroffAlumniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoroffAlumniApplication.class, args);
+	}
+
+}
