37ffab260a2aa42996b08077662f3b1835bf4c5f
Vegard46 <veg_ander@hotmail.com>
2023-03-24 11:58:19 +0100

    connected search of posts to front-end

--- noroff-alumni-frontend/src/components/post/PostFeed.tsx
+++ noroff-alumni-frontend/src/components/post/PostFeed.tsx
@@ -1,33 +1,17 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import "./post.css";
-import Topic from "../../models/Topic";
 import PostDTO from "../../models/PostDTO";
 import Post from "./Post";
-import {getPosts} from "../../services/postService";
 
 type PostFeedProps = {
-    searchWord?: string,
-    topics?: Topic[]
+    posts: PostDTO[]
 }
 
 function PostFeed(props: PostFeedProps) {
 
-    const [posts, setPosts] = useState<PostDTO[]>([]);
-
-    useEffect(() => {
-        fetchPosts();
-    }, [])
-
-    function fetchPosts(){
-        getPosts()
-            .then(data => {
-                setPosts(data);
-            })
-    }
-
     return (
         <div className="post-feed">
-            {posts.map((post, i) => (
+            {props.posts.map((post, i) => (
                 <Post key={`post-${i}`} post={post} />
             ))}
         </div>
--- noroff-alumni-frontend/src/pages/timeline/Timeline.tsx
+++ noroff-alumni-frontend/src/pages/timeline/Timeline.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import Post from "../../components/post/Post";
 import search from "../../assets/icons/Search.png";
 import "./timeline.css";
@@ -10,72 +10,11 @@ import PostMessageForm from "../../components/post-form/PostMessageForm";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import PostFeed from "../../components/post/PostFeed";
+import {getPosts, searchPosts} from "../../services/postService";
 
 const Timeline = () => {
-  const posts: PostDTO[] = [
-    {
-      id: 1,
-      title: "Lorem Ipsum",
-      last_updated: new Date(),
-      body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      target_topics: ["TOPIC 1"],
-      target_groups: ["GROUP 1"],
-      author: {firstName:"Anders",lastName:"A", id: "", email: "", username: ""},
-      comments: [
-        {
-          author:{firstName:"Marcus",lastName:"B", id: "", email: "", username: ""},
-          response: "Lorem Ipsum is simply dummy text of the printin",
-        },
-        {
-          author: {firstName:"Aleksander",lastName:"R", id: "", email: "", username: ""},
-          response: "Yes sui!",
-        },
-      ],
-    },
-    {
-      id: 2,
-      title: "Lorem Ipsum",
-      last_updated: new Date(),
-      body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-      target_topics: ["TOPIC 2"],
-      target_groups: ["GROUP 1"],
-      author: {firstName:"Anders",lastName:"A", id: "", email: "", username: ""},
-      comments: [
-        {
-          author: {firstName:"Marcus",lastName:"B", id: "", email: "", username: ""},
-          response: "Lorem Ipsum is simply dummy text of the printin",
-        },
-        {
-          author: {firstName:"Aleksander",lastName:"R", id: "", email: "", username: ""},
-          response: "Yes sui!",
-        },
-        {
-          author: {firstName:"Marcus",lastName:"B", id: "", email: "", username: ""},
-          response: "Lorem Ipsum is simply dummy text of the printin",
-        },
-        {
-          author: {firstName:"Aleksander",lastName:"R", id: "", email: "", username: ""},
-          response: "Yes sui!",
-        },
-        {
-          author: {firstName:"Marcus",lastName:"B", id: "", email: "", username: ""},
-          response: "Lorem Ipsum is simply dummy text of the printin",
-        },
-        {
-          author:{firstName:"Aleksander",lastName:"R", id: "", email: "", username: ""},
-          response: "Yes sui!",
-        },
-        {
-          author: {firstName:"Marcus",lastName:"B", id: "", email: "", username: ""},
-          response: "Lorem Ipsum is simply dummy text of the printin",
-        },
-        {
-          author: {firstName:"Aleksander",lastName:"R", id: "", email: "", username: ""},
-          response: "Yes sui!",
-        },
-      ],
-    },
-  ];
+
+  const [posts, setPosts] = useState<PostDTO[]>([]);
 
   const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
   const [activeTopic, setActiveTopic] = useState("");
@@ -84,8 +23,23 @@ const Timeline = () => {
   // Add state variable for post form popup visibility
   const [showPostForm, setShowPostForm] = useState(false);
 
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
+
+  function fetchPosts(){
+    getPosts()
+        .then(data => {
+          setPosts(data);
+        })
+  }
+
   function onSearch(searchWord: string){
-    console.log(searchWord);
+    searchPosts(searchWord)
+        .then(data => {
+          setPosts(data);
+        })
   }
 
   const handleTopicClick = (topic: string) => {
@@ -131,7 +85,7 @@ const Timeline = () => {
             </button>
           </div>
         </div>
-        <PostFeed/>
+        <PostFeed posts={posts}/>
       </div>
     </div>
   );
